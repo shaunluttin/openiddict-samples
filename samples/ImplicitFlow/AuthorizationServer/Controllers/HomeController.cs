4edd68c6dc9107725fdfc9f3c3b8bf97b5301aa1
Shaun Luttin <admin@shaunluttin.com>
2016-08-25 21:04:55 -0700

    Add HomeController

---
+++ samples/ImplicitFlow/AuthorizationServer/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
